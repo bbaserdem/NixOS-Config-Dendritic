93b2022caa80ac6f5c61d11e18bc58916c4f1e93
Batuhan Baserdem <baserdemb@gmail.com>
2026-02-27 08:35:32 -0600

    feat: package vm (umay)

--- modules/hosts/umay/flake-parts.nix
+++ modules/hosts/umay/flake-parts.nix
@@ -1,4 +1,39 @@
-{inputs, ...}: {
+{
+  lib,
+  inputs,
+  ...
+}: {
   # Create nixosConfig for our VM
   flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "umay";
+
+  # Export our image as a package
+  perSystem = {
+    pkgs,
+    system,
+    ...
+  }: {
+    packages = lib.mkMerge [
+      {}
+      (
+        lib.mkIf
+        (system == "x86_64-linux")
+        {
+          umay-vm = let
+            lib = pkgs.lib;
+            makeDiskImage = import "${inputs.nixpkgs}/nixos/lib/make-disk-image.nix";
+          in
+            makeDiskImage {
+              inherit pkgs lib;
+              config = inputs.self.nixosConfigurations.umay.config;
+              format = "qcow2";
+              onlyNixStore = false;
+              partitionTableType = "efi";
+              installBootLoader = true;
+              diskSize = "auto";
+              copyChannel = false;
+            };
+        }
+      )
+    ];
+  };
 }
