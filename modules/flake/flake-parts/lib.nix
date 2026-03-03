2b6f4de71268063839dfba38c06b0890bdb6b6b8
Batuhan Baserdem <baserdemb@gmail.com>
2026-03-03 09:35:12 -0600

    feat: update flake-parts config

--- modules/flake/flake-parts/lib.nix
+++ modules/flake/flake-parts/lib.nix
@@ -2,44 +2,59 @@
   inputs,
   lib,
   ...
-}:
-{
+}: {
   # Helper functions for creating system / home-manager configurations
 
   options.flake.lib = lib.mkOption {
     type = lib.types.attrsOf lib.types.unspecified;
-    default = { };
+    default = {};
   };
 
   config.flake.lib = {
-
-    mkNixos = system: name: {
+    # Nixos system builder, allows customizing the output name parameter
+    mkNixos = {
+      system,
+      name,
+      ...
+    }: {
       ${name} = inputs.nixpkgs.lib.nixosSystem {
         modules = [
           inputs.self.modules.nixos.${name}
-          { nixpkgs.hostPlatform = lib.mkDefault system; }
+          {nixpkgs.hostPlatform = lib.mkDefault system;}
         ];
       };
     };
 
-    mkDarwin = system: name: {
+    # Nix-Darwin config builder
+    mkDarwin = {
+      system,
+      name,
+      ...
+    }: {
       ${name} = inputs.nix-darwin.lib.darwinSystem {
         modules = [
           inputs.self.modules.darwin.${name}
-          { nixpkgs.hostPlatform = lib.mkDefault system; }
+          {nixpkgs.hostPlatform = lib.mkDefault system;}
         ];
       };
     };
 
-    mkHomeManager = system: name: {
-      ${name} = inputs.home-manager.lib.homeManagerConfiguration {
+    # Standalone home-manager config builder
+    # Not used in this flake, but possible
+    # Historically, passed host parameter to the configuration to auto-load host specific user config
+    # This is not needed with flake-parts, but host-specific behavior requires home-manager as nixos/nix-darwin module
+    mkHomeManager = {
+      system,
+      user,
+      ...
+    }: {
+      ${user} = inputs.home-manager.lib.homeManagerConfiguration {
         pkgs = inputs.nixpkgs.legacyPackages.${system};
         modules = [
-          inputs.self.modules.homeManager.${name}
-          { nixpkgs.config.allowUnfree = true; }
+          inputs.self.modules.homeManager.${user}
+          {nixpkgs.config.allowUnfree = true;}
         ];
       };
     };
-
   };
 }
