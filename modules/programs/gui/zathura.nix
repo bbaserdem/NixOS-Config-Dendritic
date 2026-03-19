0a0ecabd125ad37d2ca5cd754e5f78c9e519f519
Batuhan Baserdem <baserdemb@gmail.com>
2026-03-19 11:56:05 -0500

    feat: zathura module

---
+++ modules/programs/gui/zathura.nix
@@ -0,0 +1,28 @@
+# Zathura
+{...}: {
+  flake.modules.home-manager = {
+    # Enable stylix theming
+    stylix = {...}: {
+      stylix.targets.zathura.enable = true;
+    };
+
+    # Enable zathura
+    zathura = {...}: {
+      programs.zathura = {
+        enable = true;
+        mappings = {
+          "<C-i>" = "recolor";
+        };
+        options = {
+          recolor-keephue = true;
+          selection-keyboard = "clipboard";
+          first-page-column = "1:1";
+          incremental-search = false;
+          statusbar-home-tilde = true;
+          scroll-page-aware = true;
+          scroll-step = 50;
+        };
+      };
+    };
+  };
+}
