2373236015488eda66c35ec5acc869094ffaa59d
Batuhan Baserdem <baserdemb@gmail.com>
2026-04-09 09:36:04 -0500

    fix: ghostty darwin

--- modules/programs/dev/terminals/ghostty.nix
+++ modules/programs/dev/terminals/ghostty.nix
@@ -1,51 +1,63 @@
 # Configuring ghostty
 {...}: {
-  flake.modules.homeManager = {
-    # Enable stylix theming for ghostty
-    stylix = {...}: {
-      stylix.targets.ghostty = {
-        enable = true;
-      };
+  flake.modules = {
+    # Install through brew in nix-darwin
+    darwin.ghostty = {...}: {
+      homebrew.casks = [
+        "ghostty"
+      ];
     };
 
-    # Kitty settings
-    ghostty = {
-      pkgs,
-      lib,
-      ...
-    }: {
-      config = lib.mkMerge [
-        {
-          programs.ghostty = {
-            enable = true;
+    # Home manager configuration
+    homeManager = {
+      # Enable stylix theming for ghostty
+      stylix = {...}: {
+        stylix.targets.ghostty = {
+          enable = true;
+        };
+      };
 
-            # Integrations
-            installVimSyntax = true;
-            installBatSyntax = true;
+      # Kitty settings
+      ghostty = {
+        pkgs,
+        lib,
+        ...
+      }: {
+        config = lib.mkMerge [
+          {
+            programs.ghostty = {
+              enable = true;
 
-            # Settings
-            settings = {
+              # Integrations
+              installVimSyntax = true;
+              installBatSyntax = true;
+
+              # Settings
+              settings = {
+              };
             };
-          };
-        }
-        (lib.mkIf (pkgs.stdenv.hostPlatform.isLinux) {
-          # Linux only settings
-          programs.ghostty = {
-            systemd.enable = true;
-            settings = {
-              linux-cgroup = "single-instance";
+          }
+          (lib.mkIf (pkgs.stdenv.hostPlatform.isLinux) {
+            # Linux only settings
+            programs.ghostty = {
+              systemd.enable = true;
+              settings = {
+                linux-cgroup = "single-instance";
+              };
             };
-          };
-        })
-        (lib.mkIf (pkgs.stdenv.hostPlatform.isDarwin) {
-          # MacOS only settings
-          programs.ghostty = {
-            settings = {
-              macos-option-as-alt = true;
+          })
+          (lib.mkIf (pkgs.stdenv.hostPlatform.isDarwin) {
+            # MacOS only settings
+            programs.ghostty = {
+              # Ghostty is linux only
+              package = null;
+              settings = {
+                macos-option-as-alt = true;
+              };
             };
-          };
-        })
-      ];
+          })
+        ];
+      };
     };
   };
 }
