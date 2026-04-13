1f4cc0039de6341cbbd82de548d4a1ab2a50eec4
Batuhan Baserdem <baserdemb@gmail.com>
2026-04-13 14:13:31 -0500

    fix: direnv shell module

--- modules/programs/dev/shell/direnv.nix
+++ modules/programs/dev/shell/direnv.nix
@@ -1,23 +1,37 @@
 # Direnv setup
 {...}: {
-  flake.modules.homeManager.shell = {...}: {
-    # Enable direnv for our shells
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-      config = {
-        global = {
-          load_dotenv = false;
-          warn_timeout = "0";
+  flake.modules.homeManager.shell = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    config = lib.mkMerge [
+      {
+        # Enable direnv for our shells
+        programs.direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+          config = {
+            global = {
+              load_dotenv = false;
+              warn_timeout = "0";
+            };
+          };
         };
-      };
-    };
 
-    # Reformat direnv output to be muted
-    home.sessionVariables = let
-      logFormat = "\"$(printf '\\033[2;1;3mdirenv:\\033[22;23m %%s\\033[0m')\"";
-    in {
-      "DIRENV_LOG_FORMAT" = logFormat;
-    };
+        # Reformat direnv output to be muted
+        home.sessionVariables = let
+          logFormat = "\"$(printf '\\033[2;1;3mdirenv:\\033[22;23m %%s\\033[0m')\"";
+        in {
+          "DIRENV_LOG_FORMAT" = logFormat;
+        };
+      }
+      (
+        lib.mkIf (pkgs.stdenv.hostPlatform.isDarwin) {
+          # On nixpkgs-25.11 darwin of direnv is broken, pull from unstable
+          programs.direnv.packages = pkgs.unstable.direnv;
+        }
+      )
+    ];
   };
 }
