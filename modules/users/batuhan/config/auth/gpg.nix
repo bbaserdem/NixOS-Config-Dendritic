4e218fdab9aa9f17cd066af9dffb5e9c7905ff21
Batuhan Baserdem <baserdemb@gmail.com>
2026-04-13 08:34:54 -0500

    fix: batuhan module

--- modules/users/batuhan/config/auth/gpg.nix
+++ modules/users/batuhan/config/auth/gpg.nix
@@ -1,8 +1,25 @@
 # GPG configuration for batuhan
 {...}: {
-  flake.modules.homeManager.batuhan = {pkgs, ...}: {
-    services.gpg-agent = {
-      pinentry.package = pkgs.pinentry-gnome3;
-    };
+  flake.modules.homeManager.batuhan = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    config = lib.mkMerge [
+      (
+        lib.mkIf (pkgs.stdenv.hostPlatform.isLinux) {
+          services.gpg-agent = {
+            pinentry.package = pkgs.pinentry-gnome3;
+          };
+        }
+      )
+      (
+        lib.mkIf (pkgs.stdenv.hostPlatform.isDarwin) {
+          services.gpg-agent = {
+            pinentry.package = pkgs.pinentry_mac;
+          };
+        }
+      )
+    ];
   };
 }
