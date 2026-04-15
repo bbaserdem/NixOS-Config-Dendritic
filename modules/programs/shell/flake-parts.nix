7c30e900255e226f554c6aad3e10bca43b18be78
Batuhan Baserdem <baserdemb@gmail.com>
2026-04-14 19:26:42 -0500

    fix: module fixes

--- modules/programs/shell/flake-parts.nix
+++ modules/programs/shell/flake-parts.nix
@@ -1,5 +1,9 @@
 # Flake parts modules
-{...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   # Collect factoried modules
   flake.modules = lib.foldl lib.recursiveUpdate {} [
     (inputs.self.factory.inclusionModules "shell")
